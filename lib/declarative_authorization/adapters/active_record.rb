9cd97ec08b256bc79299be15f87b49527cbb5522
Edward Rudd <urkle@outoforder.cc>
2018-12-30 15:11:07 -0500

    remove unused and empty files

--- lib/declarative_authorization/adapters/active_record.rb
+++
@@ -1,9 +0,0 @@
-case ActiveRecord::VERSION::MAJOR
-when 3, 4
-  adapter_directory = "#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}"
-  Dir[File.expand_path("../active_record/#{adapter_directory}/*.rb", __FILE__)].each do |f|
-    require f
-  end
-else
-  raise NotImplementedError, "DeclarativeAuthorization does not support Active Record version #{ActiveRecord::VERSION::STRING}"
-end
